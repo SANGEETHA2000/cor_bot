6f7fe50d22f946a0965991d40ed734e00bafa7f5
Sangeetha V D <43891679+SANGEETHA2000@users.noreply.github.com>
2022-11-10 13:01:33 +0530

    Code Refactored

--- social_distancing.py
+++ social_distancing.py
@@ -9,19 +9,16 @@ import math
 protopath = "MobileNetSSD_deploy.prototxt"
 modelpath = "MobileNetSSD_deploy.caffemodel"
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
-# detector.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
-# detector.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
 
-tracker = CentroidTracker(maxDisappeared=40, maxDistance=50)
+tracker = CentroidTracker(max_disappeared=40, max_distance=50)
 
 
-def non_max_suppression_fast(boxes, overlapThresh):
+def non_max_suppression_fast(boxes, overlap_thresh):
     try:
         if len(boxes) == 0:
             return []
@@ -61,10 +58,44 @@ def non_max_suppression_fast(boxes, overlapThresh):
     except Exception as e:
         print("Exception occurred in non_max_suppression : {}".format(e))
 
+def person_detection(person_detections, W, H, rects)):
+    for i in np.arange(0, person_detections.shape[2]):
+            confidence = person_detections[0, 0, i, 2]
+            if confidence > 0.5:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] != "person":
+                    continue
+
+                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (start_x, start_y, end_x, end_x) = person_box.astype("int")
+                rects.append(person_box)
+     return rects
+           
+def mark_coordinates(objects, centroid_dict):
+    for (object_id, bbox) in objects.items():
+            x1, y1, x2, y2 = bbox
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            c_x = int((x1 + x2) / 2.0)
+            c_y = int((y1 + y2) / 2.0)
+            centroid_dict[objectId] = (cX, cY, x1, y1, x2, y2)
+    return centroid_dict
+
+def set_red_zone(centroid_dict):
+    for (id1, p1), (id2, p2) in combinations(centroid_dict.items(), 2):
+           dx, dy = p1[0] - p2[0], p1[1] - p2[1]
+           distance = math.sqrt(dx * dx + dy * dy)
+           if (distance < 75.0) and (id1 not in red_zone_list):
+                      red_zone_list.append(id1)
+           else:
+                      red_zone_list.append(id2)
+    return red_zone_list
 
 def main():
     cap = cv2.VideoCapture('testvideo2.mp4')
-
     fps_start_time = datetime.datetime.now()
     fps = 0
     total_frames = 0
@@ -80,48 +111,18 @@ def main():
 
         detector.setInput(blob)
         person_detections = detector.forward()
-        rects = []
-        for i in np.arange(0, person_detections.shape[2]):
-            confidence = person_detections[0, 0, i, 2]
-            if confidence > 0.5:
-                idx = int(person_detections[0, 0, i, 1])
-
-                if CLASSES[idx] != "person":
-                    continue
-
-                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-                (startX, startY, endX, endY) = person_box.astype("int")
-                rects.append(person_box)
-
+        rects = [] 
+        rects = person_detection(person_detections, W, H, rects)
+        
         boundingboxes = np.array(rects)
         boundingboxes = boundingboxes.astype(int)
         rects = non_max_suppression_fast(boundingboxes, 0.3)
         centroid_dict = dict()
         objects = tracker.update(rects)
-        for (objectId, bbox) in objects.items():
-            x1, y1, x2, y2 = bbox
-            x1 = int(x1)
-            y1 = int(y1)
-            x2 = int(x2)
-            y2 = int(y2)
-            cX = int((x1 + x2) / 2.0)
-            cY = int((y1 + y2) / 2.0)
-
-
-            centroid_dict[objectId] = (cX, cY, x1, y1, x2, y2)
-
-            # text = "ID: {}".format(objectId)
-            # cv2.putText(frame, text, (x1, y1-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+        centroid_dict = mark_coordinates(objects, centroid_dict)
 
         red_zone_list = []
-        for (id1, p1), (id2, p2) in combinations(centroid_dict.items(), 2):
-            dx, dy = p1[0] - p2[0], p1[1] - p2[1]
-            distance = math.sqrt(dx * dx + dy * dy)
-            if distance < 75.0:
-                if id1 not in red_zone_list:
-                    red_zone_list.append(id1)
-                if id2 not in red_zone_list:
-                    red_zone_list.append(id2)
+        red_zone_list = set_red_zone(centroid_dict) 
 
         for id, box in centroid_dict.items():
             if id in red_zone_list:
@@ -132,10 +133,7 @@ def main():
 
         fps_end_time = datetime.datetime.now()
         time_diff = fps_end_time - fps_start_time
-        if time_diff.seconds == 0:
-            fps = 0.0
-        else:
-            fps = (total_frames / time_diff.seconds)
+        fps = time_diff.seconds == 0 ? 0.0 : (total_frames / time_diff.seconds)
 
         fps_text = "FPS: {:.2f}".format(fps)
 
